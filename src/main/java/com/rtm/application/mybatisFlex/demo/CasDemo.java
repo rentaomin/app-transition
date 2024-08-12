80d6cb07d3ced1e332902cdf7e4f87eb3eae2719
rentaomin@wondersoft.cn <rentaomin@wondersoft.cn>
2024-08-12 09:08:24 +0800

    jut 组件java代码自动生成

--- src/main/java/com/rtm/application/mybatisFlex/demo/CasDemo.java
+++ src/main/java/com/rtm/application/mybatisFlex/demo/CasDemo.java
@@ -7,15 +7,27 @@ public class CasDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Class.forName("com.gbasedbt.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:gbasedbt-sqli://192.168.149.99:9088/bms_general_aa:GBASEDBTSERVER=dtpMng;delimident=y;SQLMODE=GBase;DB_LOCALE=zh_CN.57372;", "gbasedbt", "Ws-123456,.");
-        Statement statement = conn.createStatement();
-        String sql = "insert into t_student values (2,'admin',1)";
-        boolean execute = false;
-        try {
-            execute = statement.execute(sql);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+//        Connection conn = DriverManager.getConnection("jdbc:gbasedbt-sqli://192.168.149.99:9088/bms_general_aa:GBASEDBTSERVER=dtpMng;delimident=y;SQLMODE=GBase;DB_LOCALE=zh_CN.57372;", "gbasedbt", "Ws-123456,.");
+//        Statement statement = conn.createStatement();
+//        String sql = "insert into t_student values (2,'admin',1)";
+//        boolean execute = false;
+//        try {
+//            execute = statement.execute(sql);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println(execute);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("database test;");
+        stringBuilder.append("");
+        stringBuilder.append("INSERT INTO test:fff (name) VALUES ('测试');");
+//        try (Connection connection = DriverManager.getConnection("jdbc:gbasedbt-sqli://192.168.149.99:9098/bms_general_uba:DB_LOCALE=zh_CN.57372;DB_LOCALE=zh_CN.57372;", "gbasedbt", "Ws-123456,.");
+        try (Connection connection = DriverManager.getConnection("jdbc:gbasedbt-sqli://192.168.149.99:9098/sys:GBASEDBTSERVER=dtpInstance;DB_LOCALE=zh_CN.57372;DB_LOCALE=zh_CN.57372;", "gbasedbt", "Ws-123456,.");
+             Statement statement = connection.createStatement()) {
+            String sql = stringBuilder.toString();
+            boolean execute = statement.execute(sql);
+            System.out.println(execute);
         }
-        System.out.println(execute);
     }
 }
